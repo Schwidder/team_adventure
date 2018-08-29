b4419a0d39da4556a2dde937b8a12267b4a91462
Schwidder <kevin.schwidder1@edu.fhdw.de>
2018-08-29 14:41:41 +0200

    refs:
    - content-loader reduziert
    - Pages angepasst

--- js/content-loader.js
+++ js/content-loader.js
@@ -1,44 +1,3 @@
-function cookieController() {
-    var user = getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 365);
-        }
-    }
-    
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function loadMenu() {
-    document.getElementById("inner").setAttribute("include-html-content", "content/menu.html");
-    loadContent();
-}
-
 function loadGame() {
     document.getElementById("inner").setAttribute("include-html-content", "content/game.html");
     loadContent();
@@ -47,16 +6,6 @@ function loadGame() {
         console.log(document.getElementById("mycanvas"));
         initGame();
     }, 2000);
-}
-
-function loadImpressum() {
-    document.getElementById("inner").setAttribute("include-html-content", "content/impressum.html");
-    loadContent();
-}
-
-function loadHelp() {
-    document.getElementById("inner").setAttribute("include-html-content", "content/help.html");
-    loadContent();
 }
 
 function loadOptions() {
@@ -77,25 +26,7 @@ function setVolume() {
 function setVolumebar() {
     var audio_player = document.getElementById("audio_player");
     audio_player.volume = document.getElementById("volumecontrol").value;
-    }
-
-
-/* async function is not working
-async function getCanvas() {
-    try {
-        let content = await loadContent();
-        console.log(content);
-        if (content) {
-            console.log("wait on load success");
-            initGame();
-        }
-        else{
-            console.log("wait on load failed");
-        }
-    } catch (err) {
-        console.log(err);
-    }
-}//*/
+}
 
 function loadContent() {
     var z, i, elmnt, file, xhttp;
